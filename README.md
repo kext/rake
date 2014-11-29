c46ce42805779c5230730dd88312b8c2d74161e2
Lukas Joeressen <lukas@joeressen.net>
2014-11-29 23:58:10 +0100

    Rake scripts for C

---
+++ README.md
@@ -0,0 +1,35 @@
+# Rake scripts
+
+This is a small collection of rake scripts which help keeping the Rakefile very short.
+
+## Example
+
+```ruby
+require_relative 'rake/c'
+
+# Set some compiler flags.
+C.cflags = '-O3'
+
+# Create a library which will be placed in ‘build/libsomelibrary.a’.
+C.library 'somelibrary', [
+  # It uses the files ‘src/somelibrary/bunch.c’, ‘src/somelibrary/of.c’ and
+  # ‘src/somelibrary/files.c’.
+  'bunch.c', 'of.c', 'files.c'
+]
+
+# Set some other compiler flags.
+C.cflags = '-O3 -std=c99 -Wall -pedantic'
+
+# Make ‘build/libtest.a’ from ‘src/test/libtest.a’.
+C.library 'test', ['test.c']
+
+# Create a program ‘build/test’.
+C.program 'test', [
+  # Use the file ‘src/test.c’, ...
+  'test.c',
+  # ... the two libraries above ...
+  'libtest.a', 'libsomelibrary.a',
+  # ... and the maths library.
+  '-lm'
+]
+```
---
+++ c.rb
@@ -0,0 +1,92 @@
+require 'rake'
+
+class C
+  @@cflags = '-Wall'
+  @@compiler = ENV['CC'] || 'cc'
+  @@builddir = 'build'
+  @@sourcedir = 'src'
+  @@libs = ''
+
+  def C.run cmd
+    raise "Error: '#{cmd}'" unless system cmd
+  end
+
+  def C.clean
+    run "rm -rf \"#{@@builddir}\""
+  end
+
+  def C.cflags
+    @@cflags
+  end
+
+  def C.cflags= flags
+    @@cflags = flags
+  end
+
+  def C.library name, files
+    @@libs += " \"-I#{@@sourcedir}/#{name}\""
+    cflags = @@cflags
+    thelib = "#{@@builddir}/lib#{name}.a"
+    objects = Array.new
+    files.each do |f|
+      type = /\.[a-zA-Z0-9]+$/.match(f).to_s
+      if type == '.c' then
+        o = f.ext('.o')
+        theobject = "#{@@builddir}/objects/#{name}/#{o}"
+        thesource = "#{@@sourcedir}/#{name}/#{f}"
+        objects.push theobject
+        Rake::Task.define_task theobject => [thesource] do
+          puts "[CC] #{theobject}"
+          run "mkdir -p \"#{@@builddir}/objects/#{name}\""
+          run "#{@@compiler} #{cflags}#{@@libs} -c -o \"#{theobject}\" \"#{thesource}\""
+        end
+        Rake::Task.define_task thelib => [theobject]
+      else
+        objects.push f
+      end
+    end
+    Rake::Task.define_task thelib do
+      puts "[AR] #{thelib}"
+      run "ar rcs \"#{thelib}\" \"#{objects.join '" "'}\""
+    end
+    Rake::Task.define_task :default => thelib
+    Rake::Task.define_task :clean => :c_clean
+  end
+
+  def C.program name, files
+    cflags = @@cflags
+    theprogram = "#{@@builddir}/#{name}"
+    objects = Array.new
+    files.each do |f|
+      type = /\.[a-zA-Z0-9]+$/.match(f).to_s
+      if type == '.c' then
+        o = f.ext('.o')
+        theobject = "#{@@builddir}/objects/#{o}"
+        thesource = "#{@@sourcedir}/#{f}"
+        objects.push theobject
+        Rake::Task.define_task theobject => [thesource] do
+          puts "[CC] #{theobject}"
+          run "mkdir -p \"#{@@builddir}/objects\""
+          run "#{@@compiler} #{cflags}#{@@libs} -c -o \"#{theobject}\" \"#{thesource}\""
+        end
+        Rake::Task.define_task theprogram => [theobject]
+      elsif type == '.a' then
+        thefile = "#{@@builddir}/#{f}"
+        Rake::Task.define_task theprogram => [thefile]
+        objects.push thefile
+      else
+        objects.push f
+      end
+    end
+    Rake::Task.define_task theprogram do
+      puts "[LD] #{theprogram}"
+      run "#{@@compiler} \"-L#{@@builddir}\" -o \"#{theprogram}\" \"#{objects.join '" "'}\""
+    end
+    Rake::Task.define_task :default => theprogram
+    Rake::Task.define_task :clean => :c_clean
+  end
+end
+
+Rake::Task.define_task :c_clean do
+  C.clean
+end
